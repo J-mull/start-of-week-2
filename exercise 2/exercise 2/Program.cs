2bb5139864b6694d979830ee19d854a977b6048b
James Scadden-Mulligan <3005039@student.toiohomai.ac.nz>
2018-07-30 16:37:31 +1200

    first commit

---
+++ exercise 2/exercise 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Please enter two numbers\n Enter your first number then press enter, then enter your second number.");
+
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your two numbers added together are {0}",number1+number2);
+            Console.WriteLine("Your two numbers subtracted  are {0}", number1 - number2);
+            Console.WriteLine("Your two numbers muiltiplied are {0}", number1 * number2);
+            Console.WriteLine("Your two numbers divided are {0}", number1 / number2);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ exercise3/exercise3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter you first name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter you last name");
+            string lastName = Console.ReadLine();
+            Console.Write("Your full name is {0}",(firstName + " " +  lastName));
+
+            Console.ReadLine();
+
+        }
+    }
+}
